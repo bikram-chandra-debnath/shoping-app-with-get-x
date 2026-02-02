579f541291de6a7e21f4205bea4d0493f2da217e
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-02-02 16:03:45 +0600

    add snackbar helper

---
+++ lib/utils/popups/snackbar_helpers.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shopping_app_with_getx/utils/constants/colors.dart';
+
+class AppSnackbarHelper {
+  // warning Orange snack bar
+
+  static warnignSnackBar({required title, message = ""}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: AppColors.white,
+      backgroundColor: AppColors.warning,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.warning_2, color: AppColors.white),
+    );
+  }
+
+  // Success snack bar
+
+  static successSnackBar({required title, message = "", duration = 3}) {
+    Get.snackbar(
+      title,
+      message,
+      duration: duration,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: AppColors.white,
+      backgroundColor: AppColors.primary,
+      snackPosition: SnackPosition.BOTTOM,
+      margin: const EdgeInsets.all(10),
+      icon: const Icon(Iconsax.check, color: AppColors.white),
+    );
+  }
+
+  // Error red Snack Bar
+
+  static errorSnackBar({required title, message = ""}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: AppColors.white,
+      backgroundColor: Colors.red.shade600,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.warning_2, color: AppColors.white),
+    );
+  }
+}
